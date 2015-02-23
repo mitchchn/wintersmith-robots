ea7cdeef2d40ef542cb4770c73745c653f6a6e12
Mitchell Cohen <mitchell@magikoopa.local>
2015-02-23 17:58:54 -0500

    check if page has metadata

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ export default function (env, callback) {
 
     robots.push("User-agent: *");
     // Sitewide
-    if (noindex) {
-      robots.push("Disallow: /");
-    }
+    if (noindex) robots.push("Disallow: /");
+
     // Specific pages
     for (let page of this.pages) {
-      if (page.metadata.noindex) robots.push(`Disallow: ${page.url}`);
+      if (page.metadata) {
+        if (page.metadata.noindex) robots.push(`Disallow: ${page.url}`);
+      }
     }
-    if (sitemap) {
-      robots.push(`Sitemap: ${url}/${sitemap}`);
-    }
+    
+    if (sitemap) robots.push(`Sitemap: ${url}/${sitemap}`);
+
     callback(null, new Buffer(robots.join('\n')));
   };
 
@@ -37,13 +38,12 @@ export default function (env, callback) {
   }
 
   env.registerGenerator('robots', (contents, callback) => {
-    var pages = env.helpers.contents.list(contents).filter( (content) => {
-        return content instanceof env.plugins.MarkdownPage;
-    });
+    var pages = env.helpers.contents.list(contents).filter( (content) =>
+      content instanceof env.plugins.MarkdownPage );
 
     var tree = {'robots.txt' : new Robots(pages)};
     callback(null, tree);
   });
 
-  callback();
-};
+  callback();  
+}
