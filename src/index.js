a9b78ceb5012455d057c82b155847b826c0a7701
Mitchell Cohen <mitchell@magikoopa.local>
2015-02-23 16:08:50 -0500

    refactored

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 module.exports = (env, callback) => {
 
+  var robotsView = function (env, locals, contents, templates, callback) {
+    var robots = [],
+        {url, sitemap, noindex} = locals;
+
+    robots.push("User-agent: *");
+    // Sitewide
+    if (noindex) {
+      robots.push("Disallow: /");
+    }
+    // Specific pages
+    for (let page of this.pages) {
+      if (page.metadata.noindex) robots.push(`Disallow: ${page.url}`);
+    }
+    if (sitemap) {
+      robots.push(`Sitemap: ${url}/${sitemap}`);
+    }
+    callback(null, new Buffer(robots.join('\n')));
+  };
+
   class Robots extends env.plugins.Page {
     constructor(pages) {
       this.pages = pages;
@@ -10,30 +29,8 @@ module.exports = (env, callback) => {
     }
 
     getView() {
-      return function (env, locals, contents, templates, callback) {
-        var robots = [],
-            {sitemap, noindex} = locals;
-   
-        if (sitemap) {
-          robots.push(`Sitemap: ${locals.url}/${sitemap}`);
-        }
-     
-        robots.push("User-agent: *");
-        // Sitewide
-        if (noindex) {
-          robots.push("Disallow: /");
-        }
-        // Specific pages
-        for (let page of this.pages) {
-          if (page.metadata.noindex) {
-            robots.push(`Disallow: ${page.url}`);
-          }
-        }
-
-        callback(null, new Buffer(robots.join('\n')));
-      };
+      return robotsView;
     }
-
   }
 
   env.registerGenerator('robots', (contents, callback) => {
